671eb510a56a07cb848e3a485b917401fec58fc9
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2025-05-29 16:54:25 -0400

    initial comit

--- autoComit.bat
+++ autoComit.bat
@@ -4,6 +4,6 @@ git commit -m "initial comit "
 git push origin main
 
 :: === Tagging for GitHub Actions Release Build ===
-git tag v1.5
-git push origin v1.5
+git tag v1.6
+git push origin v1.6
 pause
